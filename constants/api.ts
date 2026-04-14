48436b2e7559d8ae3174da9e007aa3db8dd8b190
Sagnik Dey <ajaydey1946@gmail.com>
2026-04-14 21:42:50 +0530

    updates

--- constants/api.ts
+++ constants/api.ts
@@ -1,8 +1,1 @@
-import { Platform } from 'react-native';
-
-const fallbackBaseUrl = Platform.select({
-  android: 'http://10.0.2.2:4000',
-  default: 'http://localhost:4000',
-});
-
-export const API_BASE_URL = process.env.EXPO_PUBLIC_API_BASE_URL || fallbackBaseUrl || 'http://localhost:4000';
+export const API_BASE_URL = process.env.EXPO_PUBLIC_API_BASE_URL || 'https://roadgo.onrender.com';
